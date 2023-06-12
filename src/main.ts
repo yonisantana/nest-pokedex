49de847c6cab3b7c7d7ed091213a1e8ee0adf70b
Yoni Santana Milian <yoni@okboy.app>
2023-06-12 13:42:20 -0600

    PORT

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,8 @@ async function bootstrap() {
     })
   );
 
-  await app.listen(process.env.PORT || 5000);
+  //await app.listen(process.env.PORT || 5000);
+  await app.listen(process.env.PORT || 3000);
   console.log(`Aplicacion corriendo en puerto: ${process.env.PORT}`);
 }
 bootstrap();

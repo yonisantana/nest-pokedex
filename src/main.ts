8cf6f200e74734404b92f7947d1061dc6f7b4178
Yoni Santana Milian <yoni@okboy.app>
2023-06-12 00:41:21 -0600

    PORT

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-      transform: true,// transformar informacion
+      transform: true,
       transformOptions: {
         enableImplicitConversion: true
       }
